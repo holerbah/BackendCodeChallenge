8324d887252f3022d467ebbf719b8110dab22712
holerbah <holerbah@gmail.com>
2025-06-20 13:16:09 +0200

    Project initialization

---
+++ virtualPowerPlant/src/test/java/virtualPowerPlant/VirtualPowerPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package virtualPowerPlant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPowerPlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
