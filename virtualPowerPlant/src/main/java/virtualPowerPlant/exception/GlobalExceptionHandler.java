95e91390283ddbc6732cbb1a4cb08a86f41ce954
holerbah <holerbah@gmail.com>
2025-06-23 14:57:10 +0200

    Bug fix

--- virtualPowerPlant/src/main/java/virtualPowerPlant/exception/GlobalExceptionHandler.java
+++ virtualPowerPlant/src/main/java/virtualPowerPlant/exception/GlobalExceptionHandler.java
@@ -2,10 +2,8 @@ package virtualPowerPlant.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
